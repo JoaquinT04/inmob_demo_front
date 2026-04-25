b1a412200e0f443292b466af40f51eaebae41b73
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-04-25 06:08:54 -0300

    Removed useEffect in settings

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,41 +1,19 @@
 // src/pages/SettingsPage.tsx
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import CompanyForm from "@/components/settings/CompanyForm"
 import GeneralSettings from "@/components/settings/GeneralSettings"
 import UserForm from "@/components/settings/UserForm"
 import UsersList from "@/components/settings/UsersList"
-import { getTenant } from "@/services/tenant"
-import { getMe } from "@/services/auth"
+
 
 
 export default function SettingsPage() {
 const [tab, setTab] = useState<"company" | "users" | "general">("company")
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const [userRes, tenantRes] = await Promise.all([
-        getMe(),
-        getTenant(),
-      ])
 
-      setUser(userRes.data)
-      setTenant(tenantRes.data)
-      console.log("User data:", userRes.data)
-      console.log("Tenant data:", tenantRes.data)
-    } catch (err) {
-      console.error(err)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  fetchData()
-}, [])
   return (
     <div className="p-6 space-y-6">
-
       {/* Header */}
      <div className="flex gap-6 border-b border-border pb-2 text-sm">
         <button
